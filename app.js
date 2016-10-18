896ad846c261f212febc07c851b8e91e560f49e1
raghupathy23 <raghupathy0123@gmail.com>
2016-10-18 22:16:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var increment = (function () {
-    var n = 0;
-
-    return function () {
-        n += 1;
-
-        return n;
-    }
-}());
+var counter = 0;
+function increment() {
+    return counter += 1;
+}
+function counters(){
+    console.log(increment());
+	console.log(increment());
+	console.log(increment());
+	console.log(increment());
+}
 console.log("Incremented value starting from 1:");
-console.log(increment());
-console.log(increment());
-console.log(increment());
-console.log(increment());
-console.log(increment());
+counters();
